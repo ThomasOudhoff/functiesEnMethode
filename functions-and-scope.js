61bd16688d63f0c1ce2548a66a228e2e866ef4c5
ThomasOudhoff <thomasoudhoff@live.nl>
2024-10-21 14:32:50 +0200

    Functies geschreven voor huiswerk opdracht

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -2,7 +2,6 @@
 // maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
 
-const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 
 /* Opdracht  1: Cum Laude */
 
@@ -15,8 +14,17 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
-
-
+//er moet een for loop gemaakt worden die door de array heen gaat en checkt of een cijfer hoger is dan 8.
+//wanneer dit het geval is moet er een counter worden bijgehouden die aan het eind van de loop aangeeft
+//hoe veel cijfers er boven de 8 zijn en dit terug geeft.
+const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
+let counterCumLaude = 0;
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 8) {
+        counterCumLaude++;
+    }
+}
+console.log(counterCumLaude);
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
@@ -27,7 +35,18 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
-
+function cumLaude(grades) {
+    let counterCumLaude =0;
+    for (let i = 0; i < grades.length; i++) {
+        if (grades[i] >= 8) {
+            counterCumLaude++;
+        }
+    }
+    return counterCumLaude;
+}
+console.log(cumLaude([9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6]));
+console.log(cumLaude([6, 4, 5]));
+console.log(cumLaude([8, 9, 4, 6, 10]))
 
 
 /* Opdracht  2: Gemiddeld cijfer */
@@ -41,6 +60,13 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6.642857142857143
+
+//schrijf eerst een loop die door de array loopt
+//tel alle chijfers bij elkaar op en deel door array.length
+//uitkomst is het gemiddelde
+
+const average = 0;
+
 
 
 /* 2b: Omschrijven tot een herbruikbare functie */
--- functions-methods.js
+++ functions-methods.js
@@ -9,9 +9,16 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+const emailAdressSaved = "thomasoudhoff@live.nl";
 
-
-
+function getEmailDomain(){
+    searchDomain = emailAdressSaved.indexOf("@");
+    //zoekt de @ in de string
+    emailDomain = emailAdressSaved.slice(searchDomain + 1);
+    //gaat na de @ 1 indexplaats opzij.
+    return emailDomain;
+    //return alles na de @
+}
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
 // ---- Verwachte uitkomsten:
@@ -19,9 +26,25 @@
 // typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
-
-
-
+//Array met email adressen
+let emailAdresses = ["n.eeken@novi-education.nl","t.mellink@novi.nl","novi.nlaapjesk@outlook.com", "novi.nlaapjesk@outlook.com" ];
+// Functie om het type e-mail te bepalen
+function getEmail(email) {
+    if (email.endsWith("@novi.nl")) {
+        return "Medewerker"; //returned medewerker als adres eindigt op @novi.nl
+    } else if (email.endsWith("@novi-education.nl")){ //returned student als adres eindigt op @novi-edu...
+        return "Student";
+    } else {
+        return "Extern"; //in alle andere gevallen returned "extern"
+    }
+}
+    function searchEmail() {
+        for (let i = 0; i < emailAdresses.length; i++) {
+            const result = getEmail(emailAdresses[i]);
+            console.log(result);
+        }
+    }
+    searchEmail();
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
 // Een emailadres is valide wanneer:
@@ -34,3 +57,21 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+let emailAdressesValidity = ["n.eeken@novi-education.nl","t.mellink@novi.nl","n.eekenanovi.nl", "n.eeken@novinl.", "tessmellink@novi,nl"];
+function checkEmailValidity(email){
+    if (email.includes("@)")){
+        return true;
+    } if (email.includes(",")){
+        return false;
+    } if (email.endsWith(".")) {
+        return false;
+    }
+    return true;
+}
+function checkEmail() {
+    for (let i = 0; i < emailAdressesValidity.length; i++) {
+        const result = checkEmailValidity(emailAdressesValidity[i]);
+        console.log(result);
+    }
+}
+checkEmail();
